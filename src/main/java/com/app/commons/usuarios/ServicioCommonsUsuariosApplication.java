867914a0ab432e612f5a85bcce7fcaa667927933
ignacio.naranjo <ignacio.naranjo@apiservice.cl>
2020-06-14 13:11:49 -0400

    primera carga

---
+++ src/main/java/com/app/commons/usuarios/ServicioCommonsUsuariosApplication.java
@@ -0,0 +1,11 @@
+package com.app.commons.usuarios;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class ServicioCommonsUsuariosApplication {
+	
+}
